71f7ca10f21398229f567e60673e4ecf468022df
Tarau Bianca <tarau_bianca@yahoo.com>
2021-12-20 15:01:12 +0200

    Add project files.

---
+++ Tarau_Bianca_Lab10/Models/ListProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+namespace Tarau_Bianca_Lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Tarau_Bianca_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace Tarau_Bianca_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
